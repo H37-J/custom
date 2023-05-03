48f6f2b08235259b18af980b7e90cc38767ba688
hjk <these990712@gmail.com>
2023-05-03 18:05:56 +0900

    업데이트

--- src/main/java/com/hjk/strings/StringUtils.java
+++ src/main/java/com/hjk/strings/StringUtils.java
@@ -1,8 +1,24 @@
 package com.hjk.strings;
 
+import java.util.List;
+
 public class StringUtils {
 
     public static void main(String... args) {
         System.out.println("test");
+    }
+
+    public static<T> void print(T val) {
+        System.out.println(val);
+    }
+
+    public static<T> void print(T[] val) {
+        for(T v : val) {
+            print(v);
+        }
+    }
+
+    public static<T> void print(List<T> list) {
+        list.forEach(StringUtils::print);
     }
 }
--- src/main/java/com/hjk/test/TestOne.java
+++ src/main/java/com/hjk/test/TestOne.java
@@ -1,5 +1,13 @@
 package com.hjk.test;
 
+import com.hjk.strings.StringUtils;
+import io.doov.core.dsl.DOOV;
+import org.springframework.data.jpa.repository.query.QueryUtils;
+
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 import java.util.ArrayList;
 import java.util.Collections;
 
@@ -7,19 +15,39 @@ public class TestOne {
 
     private static final ArrayList<Integer> list = new ArrayList<>();
 
+
     public static void main(String... args) {
+
+    }
+
+
+
+    public static void httpTest() {
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder().
+                uri(URI.create("https://apttax.co.kr/bp.jsp"))
+                .header("Content-Type", "text/plain; charset=UTF-8")
+                .POST(HttpRequest.BodyPublishers.ofString("some body text")).build();
+
+        client.sendAsync(request, HttpResponse.BodyHandlers.ofString())
+                .thenApply(HttpResponse::body)
+                .thenAccept(System.out::println)
+                .join();
+    }
+
+
+    public void listTest() {
         list.add(1);
         list.add(2);
         list.add(3);
         list.add(4);
         list.add(5);
 
-        for (Integer test : list) {
-            System.out.println(test);
-        }
-
-        var test = Collections.max(list);
-        System.out.println(test);
+        Integer[] arr = list.stream().filter((val) -> {
+            return val > 3;
+        }).toArray(Integer[]::new);
+        StringUtils.print(arr);
     }
+
 
 }
