871a4c9b30adc6ef684cdaedb069d7f089ff93fc
these9907 <these9907@moveint.io>
2025-07-25 17:38:09 +0900

    update

---
+++ src/test/java/h/custom/CustomApplicationTests.java
@@ -0,0 +1,13 @@
+package h.custom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
